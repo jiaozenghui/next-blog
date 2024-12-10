b9fafd6ffc730d6e444215a6c783cbd054c895d0
jzh <11297332+jiaozh1@user.noreply.gitee.com>
2024-12-10 12:54:33 +0800

    feat: :sparkles: test

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -1,24 +1,73 @@
-// .commitlint.config.js
+const fs = require('node:fs')
+const path = require('node:path')
 
-module.exports= {
-    extends: ["@commitlint/config-conventional"],
-    rules: {
-      "type-enum": [
-        2,
-        "always",
-        [
-          "bug",      // 此项特别针对bug号，用于向测试反馈bug列表的bug修改情况
-          "feat",     // 新功能（feature）
-          "fix",      // 修补bug
-          "docs",     // 文档（documentation）
-          "style",    // 格式（不影响代码运行的变动）
-          "refactor", // 重构（即不是新增功能，也不是修改bug的代码变动）
-          "test",     // 增加测试
-          "chore",    // 构建过程或辅助工具的变动
-          "revert",   // feat(pencil): add ‘graphiteWidth’ option (撤销之前的commit)
-          "merge",     // 合并分支， 例如： merge（前端页面）： feature-xxxx修改线程地址
-          "release"
-        ]
-      ]
-    }
+const packages = fs.readdirSync(path.resolve(__dirname, "src"), { withFileTypes: true }).filter(dirent => dirent.isDirectory());
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [2, 'always', [...packages]]
+  },
+  prompt: {
+    messages: {
+      type: '选择你要提交的类型 :',
+      scope: '选择一个提交范围（可选）:',
+      customScope: '请输入自定义的提交范围 :',
+      subject: '填写简短精炼的变更描述 :\n',
+      body: '填写更加详细的变更描述（可选）。使用 "|" 换行 :\n',
+      breaking: '列举非兼容性重大的变更（可选）。使用 "|" 换行 :\n',
+      footerPrefixesSelect: '选择关联issue前缀（可选）:',
+      customFooterPrefix: '输入自定义issue前缀 :',
+      footer: '列举关联issue (可选) 例如: #31, #I3244 :\n',
+      generatingByAI: '正在通过 AI 生成你的提交简短描述...',
+      generatedSelectByAI: '选择一个 AI 生成的简短描述:',
+      confirmCommit: '是否提交或修改commit ?'
+    },
+    // prettier-ignore
+    types: [
+      { value: 'feat', name: '特性:     ✨  新增功能', emoji: ':sparkles:' },
+      { value: 'fix', name: '修复:     🐛  修复缺陷', emoji: ':bug:' },
+      { value: 'docs', name: '文档:     📝  文档变更', emoji: ':memo:' },
+      { value: 'style', name: '格式:     💄  代码格式（不影响功能，例如空格、分号等格式修正）', emoji: ':lipstick:' },
+      { value: 'refactor', name: '重构:     ♻️  代码重构（不包括 bug 修复、功能新增）', emoji: ':recycle:' },
+      { value: 'perf', name: '性能:     ⚡️  性能优化', emoji: ':zap:' },
+      { value: 'test', name: '测试:     ✅  添加疏漏测试或已有测试改动', emoji: ':white_check_mark:' },
+      { value: 'build', name: '构建:     📦️  构建流程、外部依赖变更（如升级 npm 包、修改 vite 配置等）', emoji: ':package:' },
+      { value: 'ci', name: '集成:     🎡  修改 CI 配置、脚本', emoji: ':ferris_wheel:' },
+      { value: 'revert', name: '回退:     ⏪️  回滚 commit', emoji: ':rewind:' },
+      { value: 'chore', name: '其他:     🔨  对构建过程或辅助工具和库的更改（不影响源文件、测试用例）', emoji: ':hammer:' }
+    ],
+    useEmoji: true,
+    emojiAlign: 'center',
+    useAI: false,
+    aiNumber: 1,
+    themeColorCode: '',
+    scopes: [],
+    allowCustomScopes: true,
+    allowEmptyScopes: true,
+    customScopesAlign: 'bottom',
+    customScopesAlias: 'custom',
+    emptyScopesAlias: 'empty',
+    upperCaseSubject: false,
+    markBreakingChangeMode: false,
+    allowBreakingChanges: ['feat', 'fix'],
+    breaklineNumber: 100,
+    breaklineChar: '|',
+    skipQuestions: [],
+    issuePrefixes: [{ value: 'closed', name: 'closed:   ISSUES has been processed' }],
+    customIssuePrefixAlign: 'top',
+    emptyIssuePrefixAlias: 'skip',
+    customIssuePrefixAlias: 'custom',
+    allowCustomIssuePrefix: true,
+    allowEmptyIssuePrefix: true,
+    confirmColorize: true,
+    maxHeaderLength: Infinity,
+    maxSubjectLength: Infinity,
+    minSubjectLength: 0,
+    scopeOverrides: undefined,
+    defaultBody: '',
+    defaultIssues: '',
+    defaultScope: '',
+    defaultSubject: ''
   }
+}
